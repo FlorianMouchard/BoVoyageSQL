ebf9a374c842210b4329443b708acd48ef8f5dfe
Blandine <blandine.ygou@live.com>
2018-06-14 13:20:50 +0200

    Modifications Interface Menu Voyage - 14062018 / 13.20

--- AppliBoVoyage/UI/ModuleGestionDossiersResa.cs
+++ AppliBoVoyage/UI/ModuleGestionDossiersResa.cs
@@ -3,10 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BoVoyage.Framework.UI;
 
 namespace AppliBoVoyage.UI
 {
     class ModuleGestionDossiersResa
     {
+        private Menu menu;
+        private SupprimerResa supprimerResa;
+        private ConsulterResa ConsulterResa;
+        private SupprimerResa SupprimerResa;
+
+        private void InitialisatonModules()
+        {
+            this.CreerResa = new CreerResa();
+            this.ConsulterResa = new ConsulterResa();
+            this.SupprimerResa = new SupprimerResa();
+        }
+
+
+        private void InitialiserMenuResa()
+        {
+            this.menu = new Menu("Gestion des dossiers de réservation");
+            this.menu.AjouterElement(new ElementMenu("1", "Consulter")
+            {
+                FonctionAExecuter = this.ConsulterResa.Demarrer
+            });
+            this.menu.AjouterElement(new ElementMenu("2", "Ajouter")
+            {
+                FonctionAExecuter = this.CreerResa.Demarrer
+            });
+            this.menu.AjouterElement(new ElementMenu("3", "Supprimer")
+            {
+                FonctionAExecuter = this.SupprimerResa.Demarrer
+            });
+            this.menu.AjouterElement(new ElementMenuQuitterMenu("R", "Revenir au menu principal..."));
+        }
+    
+
+    public void Demarrer()
+    {
+        if (this.menu == null)
+        {
+            this.InitialiserMenuResa();
+        }
+
+        this.menu.Afficher();
+    }
+
+
+
+    private void ConsulterResa()
+    {
+        ConsoleHelper.AfficherEntete("Dossier de reservation");
+
+    }
+
+
+    private void CreerResa()
+    {
+        ConsoleHelper.AfficherEntete("Nouvelle reservation");
+
+    }
+
+    private void SupprimerResa()
+    {
+        ConsoleHelper.AfficherEntete("Supprimer une reservation");
+
     }
 }
+
+
+
+
+
+
---
+++ AppliBoVoyage/UI/SousModuleAgence.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BoVoyage.Framework.UI;
+
+namespace AppliBoVoyage.UI
+{
+    class SousModuleAgence
+    {
+        private Menu menu;
+        private ModuleGestionVoyages moduleGestionVoyages;
+
+
+        private void MenuAgence()
+        {
+            this.menu = new Menu("Gestion des agences");
+            this.menu.AjouterElement(new ElementMenu("1", "Consulter agences")
+            {
+                FonctionAExecuter = this.ConsulterAgence
+            });
+            this.menu.AjouterElement(new ElementMenu("2", "Ajouter agence")
+            {
+                FonctionAExecuter = this.AjouterAgence
+            });
+            this.menu.AjouterElement(new ElementMenu("3", "Modifier agence")
+            {
+                FonctionAExecuter = this.ModifierAgence
+            });
+            this.menu.AjouterElement(new ElementMenu("4", "Supprimer agence")
+            {
+                FonctionAExecuter = this.SupprimerAgence
+            });
+            this.menu.AjouterElement(new ElementMenuQuitterMenu("R", "Revenir au menu principal..."));
+        }
+
+        public void Demarrer()
+        {
+            if (this.menu == null)
+            {
+                this.MenuAgence();
+            }
+
+            this.menu.Afficher();
+        }
+
+
+
+        private void ConsulterAgence()
+        {
+            ConsoleHelper.AfficherEntete("Les agences");
+
+
+
+        }
+
+
+        private void AjouterAgence()
+        {
+            ConsoleHelper.AfficherEntete("Nouvelle agence");
+
+        }
+
+
+        private void ModifierAgence()
+        {
+            ConsoleHelper.AfficherEntete("Modification d'une agence");
+
+        }
+
+
+        private void SupprimerAgence()
+        {
+            ConsoleHelper.AfficherEntete("Supprimer une agence");
+
+        }
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
